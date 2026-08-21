f391cc5483b2e9effd1d5bc6b69ca21ff7b76f92
wzt <3265243093@qq.com>
2026-08-21 15:28:53 +0800

    feat(navigation): add lifecycle-managed map server

---
+++ src/campusbot_navigation/launch/map_server.launch.py
@@ -0,0 +1,52 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration,PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    declare_use_sim_time = DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation/Gazebo clock',
+            )
+    map_file = LaunchConfiguration('map')
+    default_map_file = PathJoinSubstitution([FindPackageShare("campusbot_navigation"),"maps","campus_map.yaml"])
+    declare_map = DeclareLaunchArgument(
+            'map',
+            default_value=default_map_file,
+            description='Use map yaml file',
+            )
+    map_server = Node(
+            parameters=[
+                {
+                    "yaml_filename": map_file,
+                    "use_sim_time": use_sim_time
+                },
+                ],
+            package = "nav2_map_server",
+            executable="map_server",
+            name="map_server",
+            output = "screen",
+            )
+    lifecycle_manager = Node(
+            package = "nav2_lifecycle_manager",
+            executable= "lifecycle_manager",
+            name="lifecycle_manager_map_server",
+            output="screen",
+            parameters = [
+                {
+                    "use_sim_time": use_sim_time,
+                    "autostart": True,
+                    "node_names": ["map_server"],
+
+
+                    }
+                ]
+            )
+    return LaunchDescription(
+            [declare_use_sim_time,declare_map,map_server,lifecycle_manager]
+
+
+            )
+
